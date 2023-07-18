43bf84854b3680bac846e699ab74dd1fa4124c10
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-07-18 10:32:25 -0700

    Get current session profile and update the user dropdown

--- build-youtube-with-react/src/context/AuthContext.jsx
+++ build-youtube-with-react/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { supabase } from "../utils/supabase";
+import { getCurrentProfile, supabase } from "../utils/supabase";
 
 export const AuthContext = React.createContext(null);
 
@@ -7,14 +7,26 @@ export function AuthProvider({ children }) {
   const [profile, setProfile] = React.useState(null);
 
   React.useEffect(() => {
-    supabase.auth.getSession().then(({ data }) => {
-      console.log("GetSession", data.session.user);
-      setProfile(data.session.user);
+    supabase.auth.getSession().then(async ({ data }) => {
+      console.log("GetSession:", data.session.user);
+
+      if (data.session) {
+        const user = data.session.user;
+        const profile = await getCurrentProfile(user.id);
+
+        setProfile({ ...profile, user });
+      }
     });
 
-    supabase.auth.onAuthStateChange((_, session) => {
-      console.log("onAuthStateChange", session);
-      setProfile(session.user);
+    supabase.auth.onAuthStateChange(async (_, session) => {
+      console.log("onAuthStateChange:", session);
+
+      if (session) {
+        const user = session.user;
+        const profile = await getCurrentProfile(user.id);
+
+        setProfile({ ...profile, user });
+      }
     });
   }, []);
 
